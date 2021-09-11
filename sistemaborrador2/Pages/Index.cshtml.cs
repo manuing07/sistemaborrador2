0026f72b8947747c03ee5d66744699469a6ba9fa
Manuel Llancaleo <mllancaleo@gmail.com>
2021-09-11 01:23:32 -0300

    Cambios 1:23

--- sistemaborrador2/Pages/Index.cshtml.cs
+++ sistemaborrador2/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,13 @@ namespace sistemaborrador2.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        [Required (ErrorMessage ="El campo Usuario es requerido")]
+        public string Usuario { get; set; }
+        [BindProperty]
+        [Display(Name="Contraseña")]
+        [Required (ErrorMessage ="El campo Contraseña es requerido")]
+        public string Password { get; set; }
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -20,6 +28,17 @@ namespace sistemaborrador2.Pages
         public void OnGet()
         {
 
+        }
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var usuario = this.Usuario;
+            var pass = this.Password;
+
+            return Page();
         }
     }
 }
